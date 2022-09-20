68051c57397dd9291b29105093c2b3429a2561c0
OscarPrz09 <oscarprz2609@gmail.com>
2022-09-20 17:27:25 -0500

    Mi primer commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchGiftsResponse, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey : string = 'nT5OL8j0ZBBsv9AhsFiQhmQv4FFQea6s';
+  private serviciosUrl : string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[] = [];
+  
+  //Camniar any por su tipo
+  public resultados: Gif[] = []; 
+
+  get historial(){
+    return [...this._historial];
+  }
+
+  constructor( private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!);
+    //if (localStorage.getItem('historial')) {
+      //this._historial = JSON.parse(localStorage.getItem('historial')!);
+    //}
+  }
+
+  buscarGifs(query: string){
+    
+    query = query.trim().toLocaleLowerCase();
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+      
+    }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
+    this.http.get<SearchGiftsResponse>(`${this.serviciosUrl}/search`, {params})
+      .subscribe( (resp) =>{
+        this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados)); 
+      })
+  }
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+  
+  get historial(){
+    return this.giftsService.historial;
+  }
+
+  constructor(private giftsService: GifsService){};
+
+  buscar(termino: string){
+    this.giftsService.buscarGifs(termino);
+  }
+} 
